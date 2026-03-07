7d64f7ae64dcb73985b0759ad224f496498d2e74
chung-tran-dev <trandiem.9b@gmail.com>
2026-03-07 10:31:28 +0700

    Fisrt

---
+++ database/db.js
@@ -0,0 +1,18 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Trandiem244",
+    database: "healthquest"
+});
+
+db.connect((err) => {
+    if (err) {
+        console.log("Database connection error:", err);
+    } else {
+        console.log("MySQL Connected");
+    }
+});
+
+module.exports = db;
