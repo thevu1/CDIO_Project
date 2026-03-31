58357690c29f42351883e8a78dee45eda03c4777
thevu1 <149506509+thevu1@users.noreply.github.com>
2026-03-31 23:20:16 +0700

    them env cho render

--- database/db.js
+++ database/db.js
@@ -2,10 +2,13 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-
 
 module.exports = db;
